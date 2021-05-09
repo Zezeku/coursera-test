c1fbf9f5a2032859bdf77b475d4d84be7ca4943c
Zezeku <hazelnutkaffee@gmail.com>
2021-05-09 09:59:23 -0700

    testing module 5 solution

--- site/module5-solution/assignment5-solution-starter/js/script.js
+++ site/module5-solution/assignment5-solution-starter/js/script.js
@@ -108,8 +108,9 @@ console.log("ajaxUtils.sendGetRequest #2")
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
+      console.log(categories)
       var chosenCategoryShortName = chooseRandomCategory(categories)
-
+      console.log(chosenCategoryShortName)
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
